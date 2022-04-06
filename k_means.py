e02c3d133f7f664d63a47c39ebdc81c1ed86d334
Joe Shymanski <jshyman517@gmail.com>
2022-04-06 15:32:55 -0500

    Ensure each k mean is used

--- k_means.py
+++ k_means.py
@@ -11,6 +11,7 @@ def cluster(data, k, tol=.00001):
     c = np.zeros((n_exs, 1))
     while True:
         old_mean = mean.copy()
+        # i loop takes much longer than j loop
         for i in range(n_exs):
             a = [0]*k
             for j in range(k):
@@ -21,26 +22,34 @@ def cluster(data, k, tol=.00001):
             if d > 0:
                 n = np.sum((c == j)*data, axis=0)
                 mean[j] = n / d
+            else:
+                # Try to ensure all k means are used
+                print(j, mean[j])
+                mean[j] = np.random.rand(1, n_dims)*r + m
         max_change = np.amax(np.abs(old_mean - mean))
+        print(max_change)
         if max_change < tol:
             break
     return c, mean
 
 def main():
     img = Image.open("images/Mona_Lisa.jpg")
+    print(img.size)
     data = np.asarray(img)
     data = data.transpose(1, 0, 2).reshape(-1, 3)
 
     start = time.time()
-    c, mean = cluster(data, k=3)
+    c, mean = cluster(data, k=10, tol=1)
     end = time.time()
+
+    print(np.unique(c))
+    print(mean)
 
     pix = img.load()
     for i in range(data.shape[0]):
         x = i // img.size[1]
         y = i % img.size[1]
-        pix[x, y] = tuple(mean[int(c[i])].astype(int))
-
+        pix[x, y] = tuple(np.around(mean[int(c[i])]).astype(int))
     img.show()
 
     time_elapsed = end - start
